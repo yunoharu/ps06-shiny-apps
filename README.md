6e94df8c353b61c66dce1d5b2d5f8fcba9ebe456
yunoharu <yunoharu@uw.edu>
2023-03-04 15:05:32 -0800

    finish

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ps06-shiny-apps
+
+The data I used is UAH lower troposphere data, which is published bu UAH. This data contains 4 columns and 14310 rows. The variables are year, month, region, and temp (temperature). Each row represents observation, which include the information of year, month, region, and temperature.
+
+The first tab shows the overview of the data, and the main panel include the information about the number of columns and rows.\
+The second tab shows the graph about how temperatures have changed over the years, and has three widgets that allow the users to modify the visual and values of the graph. The first widget lets the users to select the type of graph (bar graph or line graph). The user can select which month's data they wish to see in the second widget, and the regions in the third widget. The main panel shows the short summary and the graph based on the selected data. The summary reacts to the widgets.\
+The third tab shows the table of the UAH data. It has two widgets, in which users can select the columns and the year range they want to see. The main panel shows the short summary and the table based on the selected data. The summary reacts to the widgets as well.
+
+Here is the link to my project: <https://yharu.shinyapps.io/ps06-shiny-apps/>
---
+++ app.R
@@ -0,0 +1,106 @@
+library(shiny)
+library(tidyverse)
+
+uah <- read_delim("UAH-lower-troposphere-long.csv.bz2")
+
+ui <- fluidPage(
+  tabsetPanel(
+    tabPanel("Overview",
+             titlePanel("UAH lower troposphere Data"),
+             mainPanel(
+               p("UAH lower trophsphere Data is downloaded from ",
+                 em("UAH."),
+                 "It has ",
+                 strong(ncol(uah)),
+                 "columns and ",
+                 strong(nrow(uah)),
+                 "rows.")
+             )
+             ),
+    tabPanel("Plot",
+             titlePanel("Lower Troposphere"),
+             sidebarLayout(
+               sidebarPanel(
+                 radioButtons("plot", label="Choose Plot Type",
+                              choices=c("Bar", "Line"),
+                              selected="Bar"),
+                 radioButtons("month", label="Choose Month",
+                             choices=c(1:12),
+                             selected=1),
+                 checkboxGroupInput("region", label="Choose Region",
+                                    choices=c("globe","globe_land","globe_ocean","nh","nh_land","nh_ocean",   
+                                              "sh","sh_land","sh_ocean","trpcs","trpcs_land","trpcs_ocean",
+                                              "noext","noext_land","noext_ocean","soext","soext_land","soext_ocean",
+                                              "nopol","nopol_land","nopol_ocean","sopol","sopol_land","sopol_ocean",
+                                              "usa48","usa49","aust"),
+                                    selected=c("globe","globe_land","globe_ocean","nh","nh_land","nh_ocean",   
+                                               "sh","sh_land","sh_ocean","trpcs","trpcs_land","trpcs_ocean",
+                                               "noext","noext_land","noext_ocean","soext","soext_land","soext_ocean",
+                                               "nopol","nopol_land","nopol_ocean","sopol","sopol_land","sopol_ocean",
+                                               "usa48","usa49","aust"))),
+             mainPanel(
+               textOutput("text1"),
+               plotOutput("plot")
+               ))),
+    tabPanel("Table",
+             titlePanel("Lower Troposphere"),
+                 sidebarLayout(
+                   sidebarPanel(
+                     checkboxGroupInput("columns", label="Select Columns to Display:",
+                                        choices = colnames(uah),
+                                        selected = colnames(uah)),
+                     sliderInput("year", label="Select Year Range",
+                                 min=min(uah$year),
+                                 max=max(uah$year),
+                                 value=c(1978,2023))),
+                   mainPanel(textOutput("text2"),
+                             tableOutput("table")
+                )))))
+
+
+server <- function(input, output){
+  output$text1 <- renderText({
+    uah %>% 
+      filter(month %in% input$month) %>%
+      filter(region %in% input$region) %>%
+      nrow() %>% 
+      paste("Selected Subset conains", ., "Obserbations")
+  })
+  output$text2 <- renderText({
+    uah %>% 
+      filter(year >= input$year[1],
+             year <= input$year[2]) %>%
+      nrow() %>% 
+      paste("Selected Subset contains", ., "Obserbations")
+  })
+  output$plot <- renderPlot({
+    if(input$plot=="Bar"){
+    uah %>% 
+      filter(month %in% input$month) %>%
+      filter(region %in% input$region) %>% 
+      group_by(year) %>% 
+      summarize(avg=mean(temp, na.rm=TRUE)) %>% 
+      ggplot(aes(x=year, y=avg, group=year))+
+      geom_col(fill="pink", col="black")+
+      labs(x="Year", y="Average Temperature")
+    }else{
+      uah %>% 
+        filter(month %in% input$month) %>%
+        filter(region %in% input$region) %>% 
+        group_by(year) %>% 
+        summarize(avg=mean(temp, na.rm=TRUE)) %>% 
+        ggplot(aes(x=year, y=avg))+
+        geom_point()+
+        geom_line()+
+        labs(x="Year", y="Average Temperature")
+    }
+  })
+  output$table <- renderTable({
+      uah %>%
+      filter(year >= input$year[1],
+             year <= input$year[2]) %>% 
+      select(input$columns)
+  })
+}
+
+shinyApp(ui = ui, server = server)
